83869440a79d8cfaf2c8b87fcad4101c274569c3
KateLockhart <katelockhart707@gmail.com>
2020-08-25 10:03:07 -0400

    Add project files.

---
+++ RestaurantRaterAPI_/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace RestaurantRaterAPI_.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ RestaurantRaterAPI_/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestaurantRaterAPI_.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
