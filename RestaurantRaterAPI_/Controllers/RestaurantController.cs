164dfbcfe905f4398d16e1c8dbd419e888d42676
KateLockhart <katelockhart707@gmail.com>
2020-08-25 15:10:58 -0400

    Added GET Methods for RestaurantController

--- RestaurantRaterAPI_/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI_/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI_.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -15,18 +16,26 @@ namespace RestaurantRaterAPI_.Controllers
         // This is where we build our endpoints for requests and responses, within the class controller
         // Endpoints are methods within the controller
 
-        RestaurantDbContext _context = new RestaurantDbContext();
+        // Field created to be read and used inside this class to hold the specific values
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
 
         // -- Create (POST)
+        // Give it an attribute to always make it a post method
+        [HttpPost]
         public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
         {
+            if(model == null)
+            {
+                return BadRequest("Your request body cannot be empty.");
+            }
+
             // ModelState is part of controller, checks for validity, activates when request is good 
             if(ModelState.IsValid)
             {
                 _context.Restaurants.Add(model);
                 await _context.SaveChangesAsync();
 
-                return Ok();
+                return Ok("Restaurant created and added to database.");
             }
             // BadRequest and Ok method give us our status codes 200 vs 400 etc
             return BadRequest(ModelState);
@@ -34,8 +43,27 @@ namespace RestaurantRaterAPI_.Controllers
 
         // -- Read (GET)
         // Get by ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+            
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
+        
 
         // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
 
         // -- Update (PUT)
 
